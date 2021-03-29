1555132a66a42ee0fd30bb9db77014646c7a96eb
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-29 15:39:31 -0400

    Try removing import to design system

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import {
   css, jsx, Global, SerializedStyles,
 } from '@emotion/react';
-import { Footer } from 'mapc-design-system';
+// import { Footer } from 'mapc-design-system';
 import Header from './Header';
 import { themeColors, fonts } from '../theme';
 
@@ -64,13 +64,13 @@ const Layout: React.FC<LayoutProps> = ({ children, cssProps }) => (
     <main css={cssProps}>
       {children}
     </main>
-    <Footer
+    {/* <Footer
       backgroundColor={themeColors.darkBlue}
       fontColor={themeColors.white}
       paddingLeft={50}
       paddingRight={50}
       maxWidth={1366}
-    />
+    /> */}
   </React.Fragment>
 );
 
