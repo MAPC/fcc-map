4424eca7cbf0f9e3b4a72eb77127f9579014bcb9
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-30 15:11:50 -0400

    Bootstrap scrollytelling map sections

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -5,6 +5,7 @@ import { css, jsx } from '@emotion/react';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import 'intersection-observer';
 import scrollama from 'scrollama';
+import image from '../images/icon.png';
 
 const sectionStyle = css`
   /* border: 1px solid red; */
@@ -44,10 +45,29 @@ const ScrollMap: React.FC = () => {
     >
       <div>
         <div className="step" data-step="a" css={sectionStyle}>
-          Text about A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. At usu errem possit patrioque, modo justo percipit sed in. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Commune platonem mel id, brute adipiscing duo an. Scripta periculis ei eam, te pro movet reformidans. Vivendum intellegat et qui, ei denique consequuntur vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Quas scaevola postulant id vis, tincidunt assueverit et pro, quo et alii scripta accommodare.
+          <h2>Section One</h2>
+          <p>
+            Introduce the idea of &quot;suburban retrofit&quot; and highlight how many spots there are on the map (over 5,000!). Each of these represents at least one parcel, but when zoomed out it&apos;s a little hard to tell how much space that is. Insert snippet about 10 square miles---that&apos;s a lot of Boston, and basically all of Woburn!
+          </p>
+          <p>
+            Most of the interactivity has been taken away from the map because it is currently serving as a static visual. If all of the controls were still present, it would be easy for the user to get &quot;off track&quot; from the story and difficult to bring them back in.
+          </p>
+          <p>
+            This can go on arbitrarily long, but we&apos;ll cut it off here. Just to show that we could include photos or charts here, though, here&apos;s the Gatsby logo:
+          </p>
+          <img src={image} alt="Gatsby logo" css={css`width: 200px;`} />
         </div>
         <div className="step" data-step="b" css={sectionStyle}>
-          Text about B. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Scripta periculis ei eam, te pro movet reformidans. Scripta periculis ei eam, te pro movet reformidans. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Saepe imperdiet at per, appareat vituperata vix te, pri sint assueverit te. Vivendum intellegat et qui, ei denique consequuntur vix. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Eos ex affert fabulas iudicabit, dolore ornatus instructior ex per. Vivendum intellegat et qui, ei denique consequuntur vix. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Dolor labitur cu pro. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Vivendum intellegat et qui, ei denique consequuntur vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te.
+          <h2>Section Two: Headings Continue</h2>
+          <p>
+            Scroll and notice the change in map view; we zoomed in and are now displaying the parcel layer instead of the circle layer. Scroll up just a little bit and the circles come back/we zoom out. This is both a joy and peril of scrollytelling: you can go forward or backward, and you need to make sure it&apos;s a digestable experience either way.
+          </p>
+          <p>
+            Again, most controls are still gone. Depending on the narrative, we might want to allow users to click and pan around, or jump to various places on the map.
+          </p>
+          <p>
+            Adding lorem ipsum here to stretch the section: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sonet cotidieque ei vel. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Dicit dicant quaestio pri eu, no principes persecuti liberavisse sit. Vivendum intellegat et qui, ei denique consequuntur vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Vivendum intellegat et qui, ei denique consequuntur vix. Scripta periculis ei eam, te pro movet reformidans. Te quo atqui libris, dicta aeque usu an. Ei qui diceret voluptua luptatum, te dolorum detracto hendrerit sed, ad per offendit consetetur. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Vivendum intellegat et qui, ei denique consequuntur vix. Cum ne case tation rationibus, nam ad iusto atomorum assueverit. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te.
+          </p>
         </div>
       </div>
       <div
@@ -61,7 +81,7 @@ const ScrollMap: React.FC = () => {
         <ReactMapGL
           {...viewport}
           width="700px"
-          height="600px"
+          height="100%"
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
           mapboxApiAccessToken="pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
           mapStyle="mapbox://styles/ihill/ckmnruq9j2aum17mabq3k5zgc"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { css, jsx } from '@emotion/react';
 import Layout from '../component/Layout';
 import ScrollMap from '../component/Map';
@@ -9,11 +8,12 @@ import { marginStyle } from '../theme';
 
 const IndexPage: React.FC = () => (
   <>
-    <Helmet>
-      {/* <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.13.0/mapbox-gl.css" rel="stylesheet" /> */}
-    </Helmet>
     <Layout cssProps={marginStyle}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nibh detracto incorrupte eos eu, iuvaret perfecto eam in, his eu possit dolorum temporibus. Te cum aeque repudiandae delicatissimi, cu populo dictas ponderum vel, dolor consequat ut vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. No vis iuvaret appareat. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Vivendum intellegat et qui, ei denique consequuntur vix. Quas scaevola postulant id vis, tincidunt assueverit et pro, quo et alii scripta accommodare. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te.
+      </p>
       <ScrollMap />
+      <p>End of scrollytelling section. We can add a filter-able version of the map here for folks to interact with, perhaps, now that they know what they're looking at.</p>
     </Layout>
   </>
 );
