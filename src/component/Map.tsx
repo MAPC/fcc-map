654f8d09349f20f5988396acbcdb56bc93a267e8
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-31 14:48:10 -0400

    Toggle between two basemaps and zoom levels

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -2,18 +2,25 @@
 
 import React, { useState, useEffect } from 'react';
 import { css, jsx } from '@emotion/react';
-import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import ReactMapGL, { Source, Layer, FlyToInterpolator } from 'react-map-gl';
 import 'intersection-observer';
 import scrollama from 'scrollama';
-import image from '../images/icon.png';
+import image from '../images/visualization.png';
 
 const sectionStyle = css`
-  /* border: 1px solid red; */
+  margin: 3rem;
+  min-height: 100vh;
 `;
 
 const ScrollMap: React.FC = () => {
   const scroller = scrollama();
   const [currentPanel, updatePanel] = useState(0);
+  const [basemap, setBasemap] = useState('mapbox://styles/ihill/ckmnruq9j2aum17mabq3k5zgc');
+  const [viewport, setViewport] = useState({
+    latitude: 42.329755482312734,
+    longitude: -71.09049350120513,
+    zoom: 8.4,
+  });
   useEffect(() => {
     scroller
       .setup({
@@ -21,20 +28,41 @@ const ScrollMap: React.FC = () => {
       })
       .onStepEnter((response) => {
         updatePanel(response.index);
+        if (response.index === 1) {
+          setViewport({
+            latitude: 42.27722101940692,
+            longitude: -71.3599202388301,
+            zoom: 11.4,
+          });
+          setBasemap('mapbox://styles/ihill/ckd4s0ptt1byo1hrlkjyn5zvy');
+        } else {
+          setViewport({
+            latitude: 42.329755482312734,
+            longitude: -71.09049350120513,
+            zoom: 8.4,
+          });
+          setBasemap('mapbox://styles/ihill/ckmnruq9j2aum17mabq3k5zgc');
+        }
       })
       .onStepExit((response) => {
         updatePanel(response.index);
+        if (response.index === 1) {
+          setViewport({
+            latitude: 42.27722101940692,
+            longitude: -71.3599202388301,
+            zoom: 11.4,
+          });
+          setBasemap('mapbox://styles/ihill/ckd4s0ptt1byo1hrlkjyn5zvy');
+        } else {
+          setViewport({
+            latitude: 42.329755482312734,
+            longitude: -71.09049350120513,
+            zoom: 8.4,
+          });
+          setBasemap('mapbox://styles/ihill/ckmnruq9j2aum17mabq3k5zgc');
+        }
       });
   }, []);
-
-  const [viewport, setViewport] = useState({
-    latitude: 42.329755482312734,
-    longitude: -71.09049350120513,
-    zoom: 8,
-  });
-
-  const displayOne = currentPanel === 0 ? 'visible' : 'none';
-  const displayTwo = currentPanel === 1 ? 'visible' : 'none';
 
   return (
     <div css={css`
@@ -53,20 +81,20 @@ const ScrollMap: React.FC = () => {
             Most of the interactivity has been taken away from the map because it is currently serving as a static visual. If all of the controls were still present, it would be easy for the user to get &quot;off track&quot; from the story and difficult to bring them back in.
           </p>
           <p>
-            This can go on arbitrarily long, but we&apos;ll cut it off here. Just to show that we could include photos or charts here, though, here&apos;s the Gatsby logo:
+            This can go on arbitrarily long, but we&apos;ll cut it off here. Just to show that we could include photos or charts here, though, here&apos;s a screencap of a Vega chart I made based off of the percentage of square miles available in each subregion:
           </p>
-          <img src={image} alt="Gatsby logo" css={css`width: 200px;`} />
+          <img src={image} alt="Pie chart of square mileage across region" css={css`width: 200px;`} />
         </div>
         <div className="step" data-step="b" css={sectionStyle}>
-          <h2>Section Two: Headings Continue</h2>
+          <h2>Section Two: Zoom Into Natick</h2>
           <p>
-            Scroll and notice the change in map view; we zoomed in and are now displaying the parcel layer instead of the circle layer. Scroll up just a little bit and the circles come back/we zoom out. This is both a joy and peril of scrollytelling: you can go forward or backward, and you need to make sure it&apos;s a digestable experience either way.
+            Scroll and notice the change in map view; we zoomed in are focused on Natick. We also changed basemaps to show some additional context. Scroll up just a little bit and the circles come back/we zoom out. This is both a joy and peril of scrollytelling: you can go forward or backward, and you need to make sure it&apos;s a digestable experience either way.
           </p>
           <p>
             Again, most controls are still gone. Depending on the narrative, we might want to allow users to click and pan around, or jump to various places on the map.
           </p>
           <p>
-            Adding lorem ipsum here to stretch the section: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sonet cotidieque ei vel. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Dicit dicant quaestio pri eu, no principes persecuti liberavisse sit. Vivendum intellegat et qui, ei denique consequuntur vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Vivendum intellegat et qui, ei denique consequuntur vix. Scripta periculis ei eam, te pro movet reformidans. Te quo atqui libris, dicta aeque usu an. Ei qui diceret voluptua luptatum, te dolorum detracto hendrerit sed, ad per offendit consetetur. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Vivendum intellegat et qui, ei denique consequuntur vix. Cum ne case tation rationibus, nam ad iusto atomorum assueverit. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te.
+            This map below provides an example of the analysis at the municipal level (Town of Natick). As expected, there are numerous suitable sites around the commuter rail stations. Less intuitive, however, is the analysis suggests some of the most suitable sites are located along Rt 9. This is likely due to larger parcels (increasing capacity potential) and the number of jobs located nearby. The maps providing a geographic and visual depiction of the suitability analysis. The analysis can also summarize for each individual parcel the various rankings and relative scores for the various criteria.
           </p>
         </div>
       </div>
@@ -80,31 +108,38 @@ const ScrollMap: React.FC = () => {
       >
         <ReactMapGL
           {...viewport}
+          transitionDuration={1000}
+          transitionInterpolator={new FlyToInterpolator()}
           width="700px"
           height="100%"
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
           mapboxApiAccessToken="pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
-          mapStyle="mapbox://styles/ihill/ckmnruq9j2aum17mabq3k5zgc"
+          mapStyle={basemap}
           scrollZoom={false}
         >
+
           <Source id="Sites" type="vector" url="mapbox://ihill.33vx5cd5">
             <Layer
               type="circle"
               id="Sites (circles)"
               source="Sites"
               source-layer="site_suitability_pt-98hifr"
-              layout={{ visibility: displayOne }}
-              paint={{ 'circle-color': '#b3b3b3', 'circle-radius': 2 }}
-            />
-          </Source>
-          <Source id="Parcels" type="vector" url="mapbox://ihill.2z6o4h6v">
-            <Layer
-              type="fill"
-              id="Sites (parcels)"
-              source="Parcels"
-              source-layer="site_geometry-9va4jv"
-              paint={{ 'fill-color': '#b3b3b3' }}
-              layout={{ visibility: displayTwo }}
+              paint={{
+                'circle-color': [
+                  'step',
+                  ['get', 'Overall_Sc'],
+                  '#FFFDA7',
+                  2,
+                  '#99f26d',
+                  2.5,
+                  '#78cd98',
+                  3,
+                  '#649abd',
+                  3.5,
+                  '#5456a0',
+                ],
+                'circle-radius': 3,
+              }}
             />
           </Source>
         </ReactMapGL>
