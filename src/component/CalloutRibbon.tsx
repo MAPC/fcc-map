e0e21cf38be9b9eabf8c9e5fa2fd7287e2d81ca4
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-04-15 10:28:42 -0400

    Roughly style callout ribbon

--- src/component/CalloutRibbon.tsx
+++ src/component/CalloutRibbon.tsx
@@ -2,30 +2,40 @@
 
 import React from 'react';
 import { css, jsx } from '@emotion/react';
-import { fonts, themeColors, marginStyle } from '../utils/theme';
+import { fonts, themeColors } from '../utils/theme';
 
 interface CalloutRibbonProps {
   background: string,
   text: string
 }
 
+const ribbonStyle = css`
+  color: ${themeColors.black};
+  display: flex;
+  flex-direction: row-reverse;
+  font-family: ${fonts.swiftNeueLtPro};
+  font-size: 3.6rem;
+  line-height: 5rem;
+  text-align: right;
+`;
+
+const ribbonTextStyle = css`
+  align-self: flex-end;
+  padding: 6rem 0;
+  width: 100rem;
+`;
+
 const CalloutRibbon: React.FC<CalloutRibbonProps> = ({ background, text }) => (
-  <aside
-    css={css`
-      ${marginStyle}
-      align-self: flex-end;
-      background: ${background};
-      color: ${themeColors.black};
-      font-family: ${fonts.swiftNeueLtPro};
-      font-size: 3.6rem;
-      line-height: 5rem;
-      padding: 6rem 10rem;
-      max-width: 120rem;
-      text-align: right;
-      width: 100rem;
-    `}
-  >
-    {text}
+  <aside css={ribbonStyle}>
+    <span css={
+      css`
+        ${ribbonTextStyle}
+        background: ${background};
+      `
+      }
+    >
+      {text}
+    </span>
   </aside>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,12 +24,7 @@ const IndexPage: React.FC = () => (
     </Helmet>
     <Layout>
       <HeroImage image={dedham} title="Dior Dedham" />
-      <div css={css`
-        background-color: ${themeColors.lightBlue};
-        display: flex;
-        flex-direction: column;
-      `}
-      >
+      <div css={css`background-color: ${themeColors.lightBlue};`}>
         <p css={middleParagraphStyle}>
           100 years ago, the land at 123 Main Street in Landborough was a pasture; in the 1960s, it was developed into a small shopping plaza; by 2000, the plaza was struggling. In 2015, a new chapter began when this land was redeveloped as a small residential building. The new apartments/condos are home to people who might otherwise not have had a chance to live in or stay in Landborough. The apartments use less energy than single family homes and didn’t require the destruction of natural areas. People who live there can walk to get a cup of coffee, or buy a gallon of milk, or to get on the bus, so they are not contributing to traffic congestion. Tax revenue is higher than it was before. By replacing a shabby retail plaza, the new building enhanced the character of the town.
         </p>
@@ -41,7 +36,10 @@ const IndexPage: React.FC = () => (
         <p css={middleParagraphStyle}>
           The region and its communities need to think strategically about these opportunities in order to maximize the benefits of commercial site retrofits. Where should we start? This report takes a comprehensive look at low-density commercial sites across the region. Using a wide range of information, we evaluated all the sites to identify those with the best potential for redevelopment, and we estimated the local and regional costs and benefits associated with reusing even some of those sites. This information is intended to help local communities begin a conversation about how they might rethink future growth in their town; and to support policy makers and state agencies who will play an important role in their reuse. Working together, these relics of the past can be transformed to meet the needs of the future.
         </p>
-        <CalloutRibbon background={themeColors.white} text="I was not aware a mixed-use development was an option with no much potential..." />
+        <CalloutRibbon background={themeColors.white} text="&quot;I was not aware a mixed-use development was an option with so much potential...&quot;" />
+        <p css={middleParagraphStyle}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Vivendum intellegat et qui, ei denique consequuntur vix. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Scripta periculis ei eam, te pro movet reformidans. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Tritani reprehendunt pro an, his ne liber iusto. Electram intellegat voluptaria et eam, eam ex aperiri temporibus scriptorem. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Qui gloriatur scribentur et, id velit verear mel, cum no porro debet.
+        </p>
       </div>
     </Layout>
   </React.Fragment>
