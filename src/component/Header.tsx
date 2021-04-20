f443d5b8c2ad972c4328bbf947a502da03f161ae
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-04-20 11:22:57 -0400

    Restyle header according to XD
    To-do if time: add stripe SVG behind text

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 import { css, jsx } from '@emotion/react';
 import { fonts, themeColors, marginStyle } from '../utils/theme';
@@ -9,10 +9,10 @@ const headerStyle = css`
   background-color: ${themeColors.indigo};
   display: flex;
   flex-direction: column;
+  position: relative;
 `;
 
 const headerWrapperStyle = css`
-  align-items: flex-end;
   display: flex;
   flex-direction: column;
   ${marginStyle}
@@ -37,12 +37,15 @@ const titleStyle = css`
   font-family: ${fonts.calibre};
   font-size: 10rem;
   font-weight: 400;
-  line-height: 14rem;
+  line-height: 10rem;
+  margin: 0;
   margin: 5rem 0 1rem 0;
 `;
 
 const underlineStyle = css`
-  border-bottom: 2rem solid ${themeColors.sky};
+  background: ${themeColors.sky};
+  color: ${themeColors.indigo};
+  padding: 1rem 1rem 0;
 `;
 
 const subtitleStyle = css`
@@ -52,33 +55,41 @@ const subtitleStyle = css`
   font-weight: normal;
   line-height: 5rem;
   margin-bottom: 12rem;
-  text-align: right;
   width: 65rem;
 `;
 
-const Header: React.FC = () => (
-  <header css={headerStyle}>
-    <div css={headerWrapperStyle}>
-      <nav css={navStyle}>
-        <Link to="/" css={navItemStyle} activeStyle={{ borderBottom: `4px solid ${themeColors.white}` }}>
-          Home
-        </Link>
-        <Link to="/about" css={navItemStyle} activeStyle={{ borderBottom: `4px solid ${themeColors.white}` }}>
-          About
-        </Link>
-      </nav>
-      <h1 css={titleStyle}>
-        <span css={underlineStyle}>
-          Rethink
-        </span>
-        {' '}
-        <span>
-          the Suburban Strip
-        </span>
-      </h1>
-      <p css={subtitleStyle}>First best place to start transforming old uses the meet the needs of residents</p>
-    </div>
-  </header>
-);
+const Header: React.FC = () => {
+  const [pageWidth, updatePageWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
+  useEffect(() => {
+    const handleResize = () => updatePageWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <header css={headerStyle}>
+      <div css={headerWrapperStyle}>
+        <nav css={navStyle}>
+          <Link to="/" css={navItemStyle} activeStyle={{ borderBottom: `4px solid ${themeColors.white}` }}>
+            Home
+          </Link>
+          <Link to="/about" css={navItemStyle} activeStyle={{ borderBottom: `4px solid ${themeColors.white}` }}>
+            About
+          </Link>
+        </nav>
+        <h1 css={titleStyle}>
+          <span css={underlineStyle}>
+            Rethink
+          </span>
+          {' '}
+          <span>
+            the Suburban Strip
+          </span>
+        </h1>
+        <p css={subtitleStyle}>First best place to start transforming old uses the meet the needs of residents</p>
+      </div>
+    </header>
+  )
+};
 
 export default Header;
