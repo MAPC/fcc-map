19dfd6e8570710dee104ac1e1ec028561ee9fe01
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2022-01-06 17:21:17 -0500

    Fix show less onclick

--- src/component/municipal-search/ExpandedSiteRow.tsx
+++ src/component/municipal-search/ExpandedSiteRow.tsx
@@ -154,8 +154,9 @@ const ExpandedSiteRow: React.FC<ExpandedSiteRowProps> = ({
       <p className="field">in the Region</p> */}
       <h3
         onClick={() => {
-          const siteIntoView:any = document.getElementById(selectedSite.site_oid);
-          siteIntoView.scrollIntoView(AlignBottom);
+          const sitesList:any = document.getElementById("sites-list");
+          const siteListItem:any = document.getElementById(selectedSite.site_oid);
+          sitesList.scrollTop = (siteListItem.offsetTop - sitesList.offsetTop);
           setSite(false);
         }}  
         css={collapseLink}
